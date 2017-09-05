afb6a8945628314fac8239f5c5924d091504a968
ShruthisMacBookAir <talk2shruthivs@gmail.com>
2017-09-05 09:56:11 +0530

    Added login and logout features

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -9,4 +10,7 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
